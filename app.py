4aab8d0460a9437e927ee155255e031afa439d33
sangameshr <sangamesh2727@gmail.com>
2023-04-28 00:41:10 +0530

    added an index.html file

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("template.html")
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
